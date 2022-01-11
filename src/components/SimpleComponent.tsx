f460d89900ddb0ca50e0dd53103abe952a41ee9e
mickwingert <mick.wingert@outlook.com>
2022-01-11 17:15:46 +1100

    Trying to pass through params

--- src/components/SimpleComponent.tsx
+++ src/components/SimpleComponent.tsx
@@ -1,15 +1,23 @@
 import { Text, Field } from '@sitecore-jss/sitecore-jss-nextjs';
+import { StyleguideComponentProps, StyleguideSpecimenFields } from 'lib/component-props';
 
-type SimpleComponentProps = {
+type StyleguideComponentParamsProps = StyleguideComponentProps & StyleguideSpecimenFields & {
     fields: {
         MainHeading: Field<string>
+    },
+    params: {
+        cssClass: string
     }
 }
 
-const SimpleComponent = (props: SimpleComponentProps): JSX.Element => (
+const SimpleComponent = (props: StyleguideComponentParamsProps): JSX.Element => {
+    const { cssClass } = props.params;
+    
+    return (
     <div>
-        <Text className="text-3xl font-bold underline" field={props.fields.MainHeading} tag="h2"/>
+        <Text className={cssClass} field={props.fields.MainHeading} tag="h2"/>
     </div>
-);
+    );
+};
 
 export default SimpleComponent;
