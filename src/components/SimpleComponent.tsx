f97c125bb11a6d3b772c07e87b974cb6d8283840
mickwingert <mick.wingert@outlook.com>
2022-01-11 15:57:39 +1100

    Updated component for className tailwind

--- src/components/SimpleComponent.tsx
+++ src/components/SimpleComponent.tsx
@@ -8,7 +8,7 @@ type SimpleComponentProps = {
 
 const SimpleComponent = (props: SimpleComponentProps): JSX.Element => (
     <div>
-        <Text field={props.fields.MainHeading} class='text-blue-600'/>
+        <Text field={props.fields.MainHeading} className="text-blue-600"/>
     </div>
 );
 
